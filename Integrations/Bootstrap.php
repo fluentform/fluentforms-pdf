ff9a9a00bb2f077ed64e24ac73a4eede68dd1859
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2020-03-13 16:35:56 +0600

    ajax handler added

--- Integrations/Bootstrap.php
+++ Integrations/Bootstrap.php
@@ -5,16 +5,23 @@ namespace FluentFormPdf\Integrations;
 use FluentForm\App\Services\Integrations\IntegrationManager;
 use FluentForm\Framework\Foundation\Application;
 use FluentForm\Framework\Helpers\ArrayHelper;
+use FluentForm\App\Modules\Acl\Acl;
 
 
 class Bootstrap
 {
-    public function __construct(Application $app)
+
+
+    protected $app = null;
+    public function __construct( Application $app)
     {
+        $this->app = $app;
+
         add_filter('fluentform_global_settings_components', array($this, 'globalSettings'));
         add_filter('fluentform_form_settings_menu', array($this, 'settingsMenu'));
-
+       
         add_action('wp_ajax_fluentform_pdf_admin_ajax_actions', array($this, 'pdfDownload'));  
+        add_action('init', array($this, 'setAjaxHandler'));
       
     }
 
@@ -37,6 +44,22 @@ class Bootstrap
         return $settingsMenus;
      }
 
+     public function setAjaxHandler() {
+        $this->app->addAdminAjaxAction(
+            'fluentform_pdf_admin_ajax_actions',
+            function () {
+                Acl::verify('fluentform_forms_manager');
+        
+              
+        
+                wp_send_json_success([
+                    'pages' => 'shamim'
+                ], 200);
+            }
+        );
+     }
+     
+
      public function pdfDownload() {
 
         if(!isset($_REQUEST['entry'])) {
@@ -45,7 +68,6 @@ class Bootstrap
      
         $userInputData = $_REQUEST['entry']["user_inputs"];
 
-        // dd($userInputData);
         $inputHtml = '';
         foreach($userInputData as $key => $value) {
             $inputHtml .=  '<p>'.$key . ': ' .$value. '</p>';
