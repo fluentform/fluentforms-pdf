072cbb3033eb5c93b2d9f630a07ce96c11f405f8
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2020-03-20 21:14:38 +0600

    Update GlobalPdfManager.php

--- Classes/Controller/GlobalPdfManager.php
+++ Classes/Controller/GlobalPdfManager.php
@@ -18,9 +18,17 @@ class GlobalPdfManager
         $this->app = $app;
 
         add_filter('fluentform_global_settings_components', array($this, 'globalSettings'));
+
+        // Global settings menu declaration
         add_filter('fluentform_form_settings_menu', array($this, 'settingsMenu'));
-        add_action('wp_ajax_fluentform_pdf_admin_ajax_actions', array($this, 'pdfDownload'));   
-        add_action('wp_ajax_fluentform_get_form_pdf_template_settings', array($this, 'getFormTemplateSettings')); 
+
+        // When download button clicked
+        add_action('wp_ajax_fluentform_pdf_admin_ajax_actions', array($this, 'pdfDownload'));
+
+        // form pdf settings fields
+        add_action('wp_ajax_fluentform_get_form_pdf_template_settings', array($this, 'getFormTemplateSettings'));
+        
+        add_action('wp_ajax_fluentform_pdf_admin_ajax_actions',array($this, 'getAllTemplates'));
         
     }
 
@@ -47,23 +55,48 @@ class GlobalPdfManager
 
     public function getAvailableTemplates() 
     {
-        $classes = apply_filters(  
+        /*
+        * key => [ path, name]
+        * For a new template this filter should use for path mapping
+        */ 
+        $classes = apply_filters(
             'fluentform_pdf_template_map',
             [
-                "template1" => "\FluentFormPdf\Classes\Templates\Template1",
-                "template2" => "\FluentFormPdf\Classes\Templates\Template2"
+                "template1" =>[
+                    'path' => "\FluentFormPdf\Classes\Templates\Template1",
+                    'name'  => 'Blank'
+                ],
+                "template2" => [
+                    'path' => "\FluentFormPdf\Classes\Templates\Template2",
+                    'name'  => 'Color' 
+                ]
             ]
         );
         return $classes;
     }
 
-    public function getFormTemplateSettings() 
+    public function getAllTemplates()
+    {
+        /*
+        * key => [ path, name]
+        * 
+        */ 
+        $classes = $this->getAvailableTemplates();
+        $allTemplates = [];
+        forEach($classes as $key => $value){    
+                $allTemplates[$key] = $value['name'];
+            
+        };
+        wp_send_json_success( $allTemplates );
+    }
+
+    public function getFormTemplateSettings()
     {
         $templateKey = $_REQUEST['templateKey'];
         $allTemplates =  $this->getAvailableTemplates();
        
-        foreach( $allTemplates as $key => $path ){
-            new $path($this->app);
+        foreach( $allTemplates as $key => $value ){
+            new $value['path']($this->app);
         };
 
         $settingsFields = apply_filters('fluentform_get_pdf_settings_fields_' . $templateKey, [], $templateKey);
@@ -81,7 +114,8 @@ class GlobalPdfManager
         $userInputData = $_REQUEST['entry']["user_inputs"];
 
         $allTemplates =  $this->getAvailableTemplates();
-        $template = new $allTemplates[$templateKey]($this->app); 
+
+        $template = new $allTemplates[$templateKey]["path"]($this->app); 
         $inputHtml = apply_filters('fluentform_get_pdf_html_template_' . $templateKey, $userInputData);
 
 
