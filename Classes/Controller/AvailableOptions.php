c1ab7795202440d5ab5add54f3b4cd36b669a2f5
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2020-03-30 12:09:45 +0600

    data structure modified

--- Classes/Controller/AvailableOptions.php
+++ Classes/Controller/AvailableOptions.php
@@ -96,6 +96,81 @@ class AvailableOptions
         ];
     }
 
+
+    public static function commonSettings() {
+        return [
+            [
+                'key'       => 'paper_size',
+                'label'     => 'Paper size',
+                'component' => 'dropdown',
+                'tab'       => 'tab2',
+                'tips'      => 'select a pdf paper size',
+                'options'   => self::getPaperSizes()
+            ],
+            [
+                'key'       => 'orientation',
+                'label'     => 'Orientation',
+                'tab'       => 'tab2',
+                'component' => 'dropdown',
+                'options'   => self::getOrientations()
+            ],
+            [
+                'key' => 'font',
+                'label' => 'Font family',
+                'component' => 'dropdown',
+                'tab'       => 'tab2',
+                'options'   => self::getFonts()
+            ],
+            [
+                'key' => 'font_color',
+                'label' => 'Font color',
+                'tab'   => 'tab2',
+                'tips'  => 'The font color will use in the PDF.',
+                'placeholder' => 'Your Feed Name',
+                'component' => 'color_picker'
+            ],
+            [
+                'key' => 'accent_color',
+                'label' => 'Accent color',
+                'tab'   => 'tab2',
+                'tips'  => 'The accent color is used for the page, section titles and the border.',
+                'placeholder' => 'Your Feed Name',
+                'component' => 'color_picker'
+            ],
+            [
+                'key' => 'entry_view',
+                'label' => 'Entry view',
+                'tab'   =>'tab2',
+                'component' => 'radio_choice',
+                'options'   => [
+                    'I' => 'View',
+                    'D' => 'Download'
+                ]
+            ],
+            [
+                'key' => 'empty_fields',
+                'label' => 'Show empty fields',
+                'tab'   =>'tab2',
+                'component' => 'radio_choice',
+                'options'   => [
+                    'yes' => 'Yes',
+                    'no' => 'No'
+                ]
+            ],
+            [
+                'key' => 'reverse_text',
+                'label' => 'Reverse text',
+                'tab'   =>'tab2',
+                'tips'   =>'Script like Arabic and Hebrew are written right to left.',
+                'component' => 'radio_choice',
+                'options'   => [
+                    'yes' => 'Yes',
+                    'no' => 'No'
+                ]
+            ]
+        ];
+    }
+
     public static function slugify($string)
     {
         return strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $string), '-'));
--- Classes/Templates/Template1.php
+++ Classes/Templates/Template1.php
@@ -21,87 +21,37 @@ class Template1 extends TemplateManager
     
     public function getSettingsFields()
     {
-        return [
-            'fields' => [
-                [
-                    'key'           => 'filename',
-                    'label'         => 'File Name',
-                    'required'      => true,
-                    'tab'           => 'tab1',
-                    'placeholder'   => 'Your File Name',
-                    'component'     => 'value_text'
-                ],
-                [
-                    'key'           => 'header',
-                    'label'         => 'Pdf header',
-                    'tab'           => 'tab1',
-                    'tips'          => 'This text will be added to the header section of pdf',
-                    'placeholder'   => 'Your Pdf Header',
-                    'component'     => 'value_text'
-                ],
-                [
-                    'key'           => 'conditionals',
-                    'label'         => 'Conditional Logics',
-                    'tab'           => 'tab1',
-                    'tips'          => 'Allow Pdf conditions',
-                    'component'     => 'conditional_block'
-                ],
-                [
-                    'key'       => 'paper_size',
-                    'label'     => 'Paper size',
-                    'component' => 'dropdown',
-                    'tab'       => 'tab2',
-                    'tips'      => 'All available templates are shown here, select a default template',
-                    'options'   => PdfOptions::getPaperSizes()
-                ],
-                [
-                    'key'       => 'orientation',
-                    'label'     => 'Orientation',
-                    'tab'       => 'tab2',
-                    'component' => 'dropdown',
-                    'options'   => PdfOptions::getOrientations()
-               ],
-                [
-                    'key' => 'font',
-                    'label' => 'Font family',
-                    'component' => 'dropdown',
-                    'tab'       => 'tab2',
-                    'options'   => PdfOptions::getFonts()
-               ],
-                [
-                    'key' => 'entry_view',
-                    'label' => 'Entry view',
-                    'tab'   =>'tab2',
-                    'component' => 'radio_choice',
-                    'options'   => [
-                        'I' => 'View',
-                        'D' => 'Download'
-                    ]
-               ],
-               [
-                    'key' => 'empty_fields',
-                    'label' => 'Show empty fields',
-                    'tab'   =>'tab2',
-                    'component' => 'radio_choice',
-                    'options'   => [
-                        'yes' => 'Yes',
-                        'no' => 'No'
-                    ]
-                ],
-                [
-                    'key' => 'reverse_text',
-                    'label' => 'Reverse text',
-                    'tab'   =>'tab2',
-                    'tips'   =>'Script like Arabic and Hebrew are written right to left.',
-                    'component' => 'radio_choice',
-                    'options'   => [
-                        'yes' => 'Yes',
-                        'no' => 'No'
-                    ]
-               ]
-
-            ]
+        $customSettings = [
+            [
+                'key'           => 'filename',
+                'label'         => 'File Name',
+                'required'      => true,
+                'tab'           => 'tab1',
+                'placeholder'   => 'Your File Name',
+                'component'     => 'value_text'
+            ],
+            [
+                'key'           => 'header',
+                'label'         => 'Pdf header',
+                'tab'           => 'tab1',
+                'tips'          => 'This text will be added to the header section of pdf',
+                'placeholder'   => 'Your Pdf Header',
+                'component'     => 'value_text'
+            ],
+            [
+                'key'           => 'conditionals',
+                'label'         => 'Conditional Logics',
+                'tab'           => 'tab1',
+                'tips'          => 'Allow Pdf conditions',
+                'component'     => 'conditional_block'
+            ],
         ];
+
+
+        return [ 'fields' => array_merge( 
+            $customSettings,
+            PdfOptions::commonSettings()
+       )];
     }
 
 
